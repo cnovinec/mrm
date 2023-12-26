056776384daa46f2cc73f57e5c1b5b10e33e0067
cnovinec <cnovinec@gmail.com>
2023-12-26 15:43:30 +1100

    Upload
    current upload to files to have the physical files on my computer to test code

---
+++ quest_func.py
@@ -0,0 +1,24 @@
+# once login verified load questions
+# with open user save data
+# if username does not exist create row in user_save_data.csv
+# with open mrmquestions.csv
+# check that username answer(i) == answer(i) in mrmquestions.csv this will tell where they are up too. if answer=='' print question
+# once question is printed then check answer with .csv if wrong error message have a pool of comments that it will choose at random to keep it interesting.
+# give a percent of 1 to be really rare
+# if answer == answer(i) write into user_save_data and run process again. (Hopefully this will work to keep program in a loop till the end)
+# build loading bar for this that works a count of answer in user... and then gets a % from mrmquestions to show the user how far they have come.
+# (loading bar might need to be a feature for this function but may need to make a main screen function that will continue to update for main program)
+# build easter egg into for Doc and Marty story key frase or askii art if date or back to future is typed in.
+# build an exit program function if user types .exit!! quit to allow Pi OS to be accesed.
+
+"Subject: Time Travel Device Activation - Security Protocol"
+
+"Hey [username],"
+
+"Doc here. I've successfully hacked into the TTD, but there's a security protocol that I have set in place."
+"Before you can get it to work, you'll need to calabrate and build it."
+"It's all part of ensuring the right hands control this powerful tech."
+"Get ready from here its up to you and your team to fix the TTD. Only the worthy will succed."
+"Let's make sure we're all on the same timeline."
+
+"Best of luck, Doc"
